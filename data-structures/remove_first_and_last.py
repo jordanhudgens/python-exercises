0cc813a4a4e11903e2d4e9230ff269711a1fe133
jordanhudgens <jordan@devcamp.com>
2018-03-28 13:32:39 -0500

    Added removal of first and last exercise

---
+++ data-structures/remove_first_and_last.py
@@ -0,0 +1,12 @@
+def remove_first_and_last(list_to_clean):
+  _, *content, _ = list_to_clean
+  return content
+
+
+html = ['<h1>', 'My content', '</h1>']
+
+print(remove_first_and_last(html))
+
+other_content_to_clean = ['', 'My content', 'Something else', '/']
+
+print(remove_first_and_last(other_content_to_clean))
