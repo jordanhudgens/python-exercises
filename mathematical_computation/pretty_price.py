e693d814a69eef0b0afadd412320bb92d7a72234
jordanhudgens <jordan@devcamp.com>
2018-04-07 23:13:40 -0500

    Added pretty price generator exercise

---
+++ mathematical_computation/pretty_price.py
@@ -0,0 +1,12 @@
+def pretty_price(gross_price, extension):
+    if isinstance(extension, int):
+        extension = extension * 0.01
+
+    return int(gross_price) + extension
+
+
+print(pretty_price(3.20, 99))
+print(pretty_price(3.99, 0.20))
+print(pretty_price(3.20, .95))
+print(pretty_price(3, 95))
+print(pretty_price(3, 2))
